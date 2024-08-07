b77d6c6f274bd377eb3c425523d1bcac82c10b82
theVicoH <huang.victor.mail@gmail.com>
2024-08-07 21:22:50 +0200

    Locomotive scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useRef } from "react";
 import NavBar from "./components/widgets/NavBar"
 import CreationsPage from "./pages/CreationsPage"
+import locomotiveScroll from "locomotive-scroll"
 
 function App() {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      const scroll = new locomotiveScroll({
+        el: scrollRef.current,
+        smooth: true,
+      });
+
+      // Optional: Clean up the effect when the component unmounts
+      return () => {
+        scroll.destroy();
+      };
+    }
+  }, []);
+
   return (
-    <div className="bg-neutral-900">
+    <div className="bg-neutral-900" ref={scrollRef}>
       <NavBar />
       <CreationsPage />
-  
     </div>
   )
 }
