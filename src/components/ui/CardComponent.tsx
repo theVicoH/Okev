18ee0ae7409a8deba1d29dfa72813df0a8d92645
theVicoH <huang.victor.mail@gmail.com>
2024-08-08 03:39:24 +0200

    Improve grid performance

--- src/components/ui/CardComponent.tsx
+++ src/components/ui/CardComponent.tsx
@@ -1,11 +1,16 @@
 import { Card } from "@/types/Card";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const CardComponent: React.FC<Card> = ({ title, description, imagePath, className }) => {
   return (
     <div className={`relative h-[520px] w-full overflow-hidden group ${className}`}>
-      <div 
-        className="absolute inset-0 bg-cover bg-center transform transition-transform duration-300 group-hover:scale-110 group-hover:brightness-50" 
-        style={{ backgroundImage: `url(${imagePath})` }} 
+      <LazyLoadImage
+        alt={title}
+        effect="blur"
+        src={imagePath}
+        wrapperClassName="absolute inset-0"
+        className="h-full w-full object-cover transform transition-transform duration-300 group-hover:scale-110 group-hover:brightness-50"
       />
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         <h2 className="font-source uppercase text-3xl font-light pb-2 text-center text-neutral-50">{title}</h2>
