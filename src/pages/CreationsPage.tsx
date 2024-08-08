d659b067f035fb41aba63b05e31e780e10f4f580
theVicoH <huang.victor.mail@gmail.com>
2024-08-08 02:21:09 +0200

    Create data for grid

--- src/pages/CreationsPage.tsx
+++ src/pages/CreationsPage.tsx
@@ -15,8 +15,7 @@ const CreationsPage = () => {
         <p className="mx-4 max-w-screen-md py-20 text-xl md:w-full md:mx-0">
           Okev est une agence multitalent. Notre fonctionnement s'inspire de la
           minutie, de l'attention au détail, et de l'artisanat raffiné typique
-          de l'industrie du luxe. Notre mission ? Créer des émotions en
-          racontant des histoires chargées de sens et d’esprit pour porter haut
+          de l'industrie du luxe.<br/> Notre mission ? Créer des émotions en racontant des histoires chargées de sens et d’esprit pour porter haut
           et fort le message que vous voulez délivrer, créer une image de marque
           créative ou encore développer une stratégie marketing impactante.
         </p>
